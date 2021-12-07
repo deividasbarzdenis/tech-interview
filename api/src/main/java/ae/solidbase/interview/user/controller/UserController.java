25b217a4d8d86dcde46df2ad8d03b0e930da6696
deividasbardzenis <deividasbarzdenis@yahoo.com>
2021-12-07 19:19:46 +0200

    created User model assembler for data transformation from entity to dto.

--- api/src/main/java/ae/solidbase/interview/user/controller/UserController.java
+++ api/src/main/java/ae/solidbase/interview/user/controller/UserController.java
@@ -1,7 +1,11 @@
 package ae.solidbase.interview.user.controller;
 
 
+import lombok.AllArgsConstructor;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+@AllArgsConstructor
+@RepositoryRestController
 public class UserController {
 
 }
--- api/src/main/java/ae/solidbase/interview/user/projection/UserModelAssembler.java
+++ api/src/main/java/ae/solidbase/interview/user/projection/UserModelAssembler.java
@@ -1,4 +1,22 @@
 package ae.solidbase.interview.user.projection;
 
-public class UserModelAssembler {
+import ae.solidbase.interview.user.controller.UserController;
+import ae.solidbase.interview.user.model.User;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+
+public class UserModelAssembler extends RepresentationModelAssemblerSupport<User, UserModel> {
+
+    public UserModelAssembler() {
+        super(UserController.class, UserModel.class);
+    }
+
+    @Override
+    protected UserModel instantiateModel(User user) {
+        return new UserModel(user);
+    }
+
+    @Override
+    public UserModel toModel(User user) {
+        return createModelWithId(user.getId(), user);
+    }
 }
